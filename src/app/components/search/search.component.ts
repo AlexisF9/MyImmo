d5ac3ec4ad70f53367eb05ffa816c6903ffdfa22
Alexis Flacher <alexis.flacher38@gmail.com>
2025-05-11 18:19:00 +0200

    edit search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [MatTabsModule, MatIconModule, MatInputModule],
+  imports: [MatTabsModule, MatIconModule, MatInputModule, FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-
+  search = model<string>("")
 }
