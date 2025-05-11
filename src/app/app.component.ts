824522f53de9d322dd9f1246837012b02e9e585e
Alexis Flacher <alexis.flacher38@gmail.com>
2025-05-11 13:18:42 +0200

    create header component + add background picture in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { CardsListComponent } from "../../components/cards-list/cards-list.component";
+import {MatTabsModule} from '@angular/material/tabs';
 
 export interface Advertisement {
   id: number,
@@ -23,12 +24,15 @@ export interface Advertisement {
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, CardsListComponent],
+  imports: [MatGridListModule, CardsListComponent, MatTabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   data: Advertisement[] | null = null;
+
+  filteredBuyData: Advertisement[] | null = null
+  filteredRentData: Advertisement[] | null = null
 
   constructor(private apiService: ApiService) {}
 
@@ -36,9 +40,11 @@ export class HomeComponent {
     this.apiService.getData().subscribe({
       next: (res) => {
         this.data = res.data
-        console.log(this.data)
+        this.filteredBuyData = this.data?.filter((item: Advertisement) => item.distribution_type.name === "Acheter") ?? null
+        this.filteredRentData = this.data?.filter((item: Advertisement) => item.distribution_type.name === "Louer") ?? null
       },
       error: (err) => console.error('Erreur API:', err)
     });
   }
+
 }
