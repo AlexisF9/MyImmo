eb8ea79994df3c797663980bfb9583e3171a4d02
AlexisF9 <alexis.flacher38@gmail.com>
2025-05-11 14:33:01 +0200

    create footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, HeaderComponent],
+  imports: [RouterOutlet, HttpClientModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
