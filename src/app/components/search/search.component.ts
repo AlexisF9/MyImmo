399e4523a8ae060d88e9480dcb1abfeb8718cdc0
Alexis Flacher <alexis.flacher38@gmail.com>
2025-05-11 19:51:01 +0200

    edit search component + edit api service

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,73 @@
-import { Component, model } from '@angular/core';
+import { Component, ElementRef, HostListener, model, ViewChild } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ApiService } from '../../services/api.service';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  imports: [MatTabsModule, MatIconModule, MatInputModule, FormsModule],
+  imports: [MatTabsModule, MatIconModule, MatInputModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-  search = model<string>("")
+  searchControl = new FormControl();
+  search: string = ""
+  data: {city: string, postale_code: string}[] | null = null;
+  openResults: boolean = false
+
+  constructor(private apiService: ApiService) {}
+
+  @ViewChild('resultsList') resultsListRef!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onClickDocument(event: MouseEvent): void {
+    const clickedInside = this.resultsListRef?.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.openResults = false
+    }
+  }
+
+  changeSearchControl(city: string, cp: string) {
+    this.searchControl.setValue(`${city} (${cp})`)
+    this.data = null
+
+    this.apiService.getAdvertisementsByAddress(city).subscribe({
+      next: (res) => {
+        console.log(res.data)
+      },
+      error: (err) => {
+        console.error('Erreur API:', err)
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.searchControl.valueChanges.pipe(debounceTime(300)).subscribe(data => {
+      this.searchAddress(data)
+    });
+  }
+  
+  searchAddress(search: string) {
+    if (search.length >= 3) {
+      const url = `https://strapi-server-0ymt.onrender.com/api/addresses?filters[$or][0][city][$containsi]=${search}&filters[$or][1][postale_code][$containsi]=${search}&pagination[limit]=10`
+      this.apiService.getAddress(url).subscribe({
+        next: (res) => {
+          this.data = res.data
+          if (this.data && this.data?.length > 0) {
+            this.openResults = true
+          }
+        },
+        error: (err) => {
+          console.error('Erreur API:', err)
+          this.data = null
+        }
+      });
+    } else {
+      this.data = null
+    }
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,11 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'https://strapi-server-0ymt.onrender.com/api/advertisements?populate=*';
+  private apiUrl = 'https://strapi-server-0ymt.onrender.com';
 
   constructor(private http: HttpClient) {}
 
   getData(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl + '/api/advertisements?populate=*');
+  }
+
+  getAddress(url: string): Observable<any> {
+    return this.http.get<any>(url);
+  }
+
+  getAdvertisementsByAddress(city: string): Observable<any> {
+    return this.http.get<any>(this.apiUrl + `/api/advertisements?filters[address][city][$eq]=${city}&populate=address`);
   }
 }
