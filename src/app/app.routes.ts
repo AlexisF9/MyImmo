f0fa1cf312ded0f072baaf32777db8a76b688bca
Alexis Flacher <alexis.flacher38@gmail.com>
2025-05-12 11:53:06 +0200

    create not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchComponent } from './pages/search/search.component';
 import { AnnouncementComponent } from './pages/announcement/announcement.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'annonce/:id',
         component: AnnouncementComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
