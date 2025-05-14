14ee4a85878bef371ab46bf2a5ba9241c61cc11e
AlexisF9 <alexis.flacher38@gmail.com>
2025-05-14 19:39:54 +0200

    add keen slider lib + add pictures slider in mobile in pictures component

--- src/app/components/pictures/pictures.component.ts
+++ src/app/components/pictures/pictures.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
-import { ChevronLeft, LucideAngularModule } from 'lucide-angular';
+import { Component, ElementRef, input, Renderer2, ViewChild } from '@angular/core';
+import { Camera, ChevronLeft, ChevronRight, LucideAngularModule } from 'lucide-angular';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
   selector: 'app-pictures',
@@ -8,16 +9,69 @@ import { ChevronLeft, LucideAngularModule } from 'lucide-angular';
   styleUrl: './pictures.component.scss'
 })
 export class PicturesComponent {
-  readonly ArrowIcon = ChevronLeft;
+  readonly ChevronLeftIcon = ChevronLeft;
+  readonly ChevronRightIcon = ChevronRight;
+  readonly CameraIcon = Camera;
   pictures = input.required<{url: string, name: string}[]>()
   zoom: boolean = false
+  
+  screenWidth!: number;
+  isMobile!: boolean;
+  resizeListener!: () => void;
+
+  constructor(private renderer: Renderer2) {}
+
+  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
+
+  currentSlide: number = 0
+  slider: KeenSliderInstance | null = null
+
+  ngAfterViewInit() {
+    if (this.sliderRef) {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        initial: this.currentSlide,
+        slideChanged: (s) => {
+          this.currentSlide = s.track.details.rel
+        },
+      })
+    }
+  }
 
   ngOnInit() {
-    console.log(this.pictures())
+    this.screenWidth = window.innerWidth;
+    this.isMobile = this.screenWidth <= 768; 
+
+    this.resizeListener = this.renderer.listen('window', 'resize', () => {
+      this.onResize();
+
+      if (this.isMobile && this.sliderRef) {
+        this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+          initial: this.currentSlide,
+          slideChanged: (s) => {
+            this.currentSlide = s.track.details.rel
+          },
+        })
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.slider) {
+      this.slider.destroy()
+    }
+
+    if (this.resizeListener) {
+      this.resizeListener();
+    }
   }
 
   toggleZoom() {
     this.zoom = !this.zoom
     document.body.classList.toggle('noscroll');
+  }
+
+  onResize() {
+    this.screenWidth = window.innerWidth;
+    this.isMobile = this.screenWidth <= 768;
   }
 }
