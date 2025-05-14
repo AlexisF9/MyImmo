21dec4c0947614f5d9ebcfeefb288183a1b084c4
AlexisF9 <alexis.flacher38@gmail.com>
2025-05-14 21:03:52 +0200

    start like system with localstorage in card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,14 +1,41 @@
 import { Component, input } from '@angular/core';
 import { Announcement } from '../../pages/home/home.component';
 import { RouterLink } from '@angular/router';
+import { Heart, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-card',
-  imports: [ RouterLink ],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
   announcement = input.required<Announcement>()
   ceil = Math.ceil
+  readonly HeartIcon = Heart
+  likesList: number[] = []
+
+  ngOnInit() {
+    if (localStorage.getItem("likes")) {
+      this.likesList = JSON.parse(localStorage.getItem("likes") as string)
+    }
+  }
+
+  toggleLike(id: number) {
+    if (this.likesList.find((el) => el === id)) {
+      const index = this.likesList.indexOf(id)
+      this.likesList.splice(index, 1)
+      localStorage.setItem("likes", JSON.stringify(this.likesList))
+    } else {
+      this.likesList.push(id)
+      localStorage.setItem("likes", JSON.stringify(this.likesList))
+    }
+  }
+
+  likeClass(id: number) {
+    if (this.likesList.find((el) => el === id)) {
+      return "fill-red-600 stroke-red-600"
+    }
+    return;
+  }
 }
