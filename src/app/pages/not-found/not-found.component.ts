8c964120c7279be35391ee70beec2b3fd0787e7d
AlexisF9 <alexis.flacher38@gmail.com>
2025-05-15 13:18:30 +0200

    edit not found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../components/button/button.component";
+import { LucideAngularModule, Search } from 'lucide-angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [ButtonComponent, LucideAngularModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+  readonly SearchIcon = Search
 
+  constructor(private router: Router) {}
+
+  redirectToHome() {
+    this.router.navigate(['/']);
+  }
 }
